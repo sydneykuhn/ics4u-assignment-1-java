d57d0f6f71985ad5d54d7b1eea433c04aeba143a
Sydney Kuhn <sydney.kuhn@mths.ca>
2022-09-20 23:08:00 +0000

    added rps

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,83 @@
+/*
+*  The program plays rock, paper, scissors
+*    with user input.
+*
+* @author  Sydney
+* @version 1.0
+* @since   2022-9-19
+*/
+
+import java.io.Console;
+
+/**
+* This program plays rock, paper, scissors.
+*/
+final class RockPaperScissors {
+    /**
+     * Set constant rock.
+     */
+    private static final int ROCK = 1;
+
+    /**
+     * Set constant paper.
+     */
+    private static final int PAPER = 2;
+
+    /**
+     * Set constant scissor.
+     */
+    private static final int SCISSOR = 3;
+
+    /**
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private RockPaperScissors() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+
+        // Get user input
+        final Console console = System.console();
+        System.out.println("Let's play Rock, Paper, Scissors!");
+        System.out.println("Rock (1)\nPaper (2)\nScissors (3)");
+        final String userInputString = console.readLine(
+            "Enter a number 1, 2 or 3 to chose : ");
+
+        final int userInput = Integer.valueOf(userInputString);
+
+        // Get random int
+        final int min = 1;
+        final int max = 3;
+        final int randomInt = (int) (Math.random() * max + min);
+
+        // If statements
+        if (userInput == randomInt) {
+            System.out.println("You tied!");
+        } else if (userInput == ROCK & randomInt == PAPER) {
+            System.out.println("You lose!");
+        } else if (userInput == ROCK & randomInt == SCISSOR) {
+            System.out.println("You win!");
+        } else if (userInput == PAPER & randomInt == ROCK) {
+            System.out.println("You win!");
+        } else if (userInput == PAPER & randomInt == SCISSOR) {
+            System.out.println("You lose!");
+        } else if (userInput == SCISSOR & randomInt == ROCK) {
+            System.out.println("You lose!");
+        } else if (userInput == SCISSOR & randomInt == PAPER) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("\nInvalid Input Entered.\nDone.");
+        }
+        System.out.println("\nDone.");
+    }
+}
